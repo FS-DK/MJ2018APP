a564f6b8bf21651684e80ff8db2abc0f9b6dd45b
FS-DK <frants@sandbergfamilien.dk>
2018-05-17 21:35:44 +0200

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -85,3 +85,19 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/bus');
 });
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("9733519c-b68f-4fde-a3a8-cc1551313e83")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
